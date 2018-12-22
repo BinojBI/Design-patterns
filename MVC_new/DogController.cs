48acf5872fd6c88daa825f554c1eedc947528352
BinojBI <binojishara21@gmail.com>
2018-12-23 00:43:30 +0530

    MVC easy undeerstanding

---
+++ MVC_new/DogController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_new
+{
+    class DogController
+    {
+        private Dog model;
+        private DogView view;
+
+        public DogController(Dog model, DogView view)
+        {
+            this.model = model;
+            this.view = view;
+        }
+
+        public void setName(String name)
+        {
+            model.name = name;
+        }
+        public void setAge(int age)
+        {
+            model.age = age;
+        }
+        public String getName()
+        {
+            return model.name;
+        }
+        public int getAge()
+        {
+            return model.age;
+        }
+        public void updateView()
+        {
+            view.printDogInfo(model.name, model.age);
+        }
+    }
+}
